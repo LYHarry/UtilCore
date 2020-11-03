894a1a7f9ac694ae720ac8a42f7f8d9dfb6daf40
LYHarry <harryliubing@sina.com>
2020-11-03 10:24:31 +0800

    添加base64加密

---
+++ Easycode.Common/DEncrypt/Base64Cipher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Official.Infrastructure.Utils.DEncrypt
+{
+    public class Base64Cipher
+    {
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="content">待加密字符串</param>
+        /// <param name="encoding">编码</param>
+        /// <returns></returns>
+        public static string Encrypt(string content, Encoding encoding)
+        {
+            byte[] bytes = encoding.GetBytes(content);
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="content">待加密字符串</param>
+        /// <returns></returns>
+        public static string Encrypt(string content) => Encrypt(content, Encoding.UTF8);
+
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="content">待解密字符串</param>
+        /// <param name="encoding">编码</param>
+        /// <returns></returns>
+        public static string Decrypt(string content, Encoding encoding)
+        {
+            byte[] bytes = Convert.FromBase64String(content);
+            return encoding.GetString(bytes);
+        }
+
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="content">待解密字符串</param>
+        /// <returns></returns>
+        public static string Decrypt(string content) => Decrypt(content, Encoding.UTF8);
+
+    }
+}
